1441ec9a17542ae6357d75245ecd635ee9666780
Fernando Cejas <fcejas@gmail.com>
2014-10-10 22:02:59 +0200

    Add documentation to Gandalf Strict Mode annotation.

--- gandalf-library/src/main/java/com/fernandocejas/android10/gandalf/annotation/GStrictMode.java
+++ gandalf-library/src/main/java/com/fernandocejas/android10/gandalf/annotation/GStrictMode.java
@@ -11,6 +11,20 @@ import static java.lang.annotation.ElementType.CONSTRUCTOR;
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.RetentionPolicy.CLASS;
 
+/**
+ * <br>Gandalf Strict Mode enables Android {@link android.os.StrictMode} on your application, so
+ * when violations are detected, the logcat output will show information about these ones as they
+ * happen.<br>
+ *
+ * <br>REMEMBER: It is not necessary to annotate each method/constructor, it is enough with
+ * just using the annotation on the main entry point of your app to activate
+ * {@link android.os.StrictMode}. Thus this feature will live along the execution of your app.<br>
+ *
+ * <br>FOR EXAMPLE: It is a good practice to annotate
+ * {@link android.app.Activity#onCreate(android.os.Bundle)} method of your main activity or
+ * {@link android.app.Application#onCreate()} method of your {@link android.app.Application} class
+ * so the violations can be detected as they happen along the execution of your app.
+ */
 @Retention(CLASS)
 @Target({ CONSTRUCTOR, METHOD })
 public @interface GStrictMode {}
