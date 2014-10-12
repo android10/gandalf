5e182245ce8d59ca3fe48263d204aa62b342cb7d
Fernando Cejas <fcejas@gmail.com>
2014-10-12 19:12:00 +0200

    Remove unused sample code.

--- gandalf-example/src/main/java/com/example/gandalf/MainActivity.java
+++ gandalf-example/src/main/java/com/example/gandalf/MainActivity.java
@@ -2,14 +2,7 @@ package com.example.gandalf;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import com.fernandocejas.android10.gandalf.Gandalf;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 public class MainActivity extends Activity {
 
@@ -38,47 +31,10 @@ public class MainActivity extends Activity {
     dataPresenterOne.printMessage("this is tag", "this is message");
 
     dataPresenterOne.executeDiskIOTaskOnUiThread();
-
-    //this.executeNetworkTaskOnUiThread();
-    //this.executeDiskIOTaskOnUiThread();
   }
 
   @Override protected void onDestroy() {
     super.onDestroy();
-
-    //dataPresenterOne.executeDiskIOTaskOnUiThread();
-
     Gandalf.printStats();
-  }
-
-  private void executeNetworkTaskOnUiThread() {
-    try {
-      URL url = new URL("http://www.fernandocejas.com");
-      HttpURLConnection con = (HttpURLConnection) url.openConnection();
-      readStream(con.getInputStream());
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-  }
-
-  private void readStream(InputStream in) {
-    BufferedReader reader = null;
-    try {
-      reader = new BufferedReader(new InputStreamReader(in));
-      String line = "";
-      while ((line = reader.readLine()) != null) {
-        Log.d("Penano", "Lineeee ----> " + line);
-      }
-    } catch (IOException e) {
-      e.printStackTrace();
-    } finally {
-      if (reader != null) {
-        try {
-          reader.close();
-        } catch (IOException e) {
-          e.printStackTrace();
-        }
-      }
-    }
   }
 }
