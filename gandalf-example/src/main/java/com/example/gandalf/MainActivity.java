973912e6d5e67ce02b065ac177525e1f05481482
Fernando Cejas <fcejas@gmail.com>
2014-10-23 22:56:25 +0200

    Add activity to show the capabilities of the library.

--- gandalf-example/src/main/java/com/example/gandalf/MainActivity.java
+++ gandalf-example/src/main/java/com/example/gandalf/MainActivity.java
@@ -1,10 +1,10 @@
 package com.example.gandalf;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import com.fernandocejas.android10.gandalf.Gandalf;
 
 public class MainActivity extends Activity {
 
@@ -25,13 +25,8 @@ public class MainActivity extends Activity {
 
   private final View.OnClickListener loadExamplesOnClickListener = new View.OnClickListener() {
     @Override public void onClick(View v) {
-      //Intent intent = new Intent();
-      //startActivity(intent);
+      Intent intent = new Intent(MainActivity.this, SamplesActivity.class);
+      startActivity(intent);
     }
   };
-
-  @Override protected void onDestroy() {
-    super.onDestroy();
-    Gandalf.printStats();
-  }
 }
---
+++ gandalf-example/src/main/java/com/example/gandalf/SamplesActivity.java
@@ -0,0 +1,72 @@
+package com.example.gandalf;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import com.fernandocejas.android10.gandalf.Gandalf;
+
+public class SamplesActivity extends Activity {
+
+  private Button btn_sampleGLogAnnotation;
+  private Button btn_sampleGTraceAnnotation;
+  private Button btn_sampleGWatchAnnotation;
+  private Button btn_sampleStrictModeAnnotation;
+  private Button btn_sampleGandalfStats;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_samples);
+    this.mapGUI();
+  }
+
+  private void mapGUI() {
+    this.btn_sampleGLogAnnotation = (Button)findViewById(R.id.btn_sampleGLogAnnotation);
+    this.btn_sampleGTraceAnnotation = (Button)findViewById(R.id.btn_sampleGTraceAnnotation);
+    this.btn_sampleGWatchAnnotation = (Button)findViewById(R.id.btn_sampleGWatchAnnotation);
+    this.btn_sampleStrictModeAnnotation = (Button)findViewById(R.id.btn_sampleStrictModeAnnotation);
+    this.btn_sampleGandalfStats = (Button)findViewById(R.id.btn_sampleGandalfStats);
+
+    this.btn_sampleGLogAnnotation.setOnClickListener(buttonSampleGLogListener);
+    this.btn_sampleGTraceAnnotation.setOnClickListener(buttonSampleGTraceListener);
+    this.btn_sampleGWatchAnnotation.setOnClickListener(buttonSampleGWatchListener);
+    this.btn_sampleStrictModeAnnotation.setOnClickListener(buttonSampleStrictModeListener);
+    this.btn_sampleGandalfStats.setOnClickListener(buttonSampleGandalfStatsListener);
+  }
+
+  @Override protected void onDestroy() {
+    super.onDestroy();
+    Gandalf.printStats();
+  }
+
+  private View.OnClickListener buttonSampleGLogListener = new View.OnClickListener() {
+    @Override public void onClick(View v) {
+
+    }
+  };
+
+  private View.OnClickListener buttonSampleGTraceListener = new View.OnClickListener() {
+    @Override public void onClick(View v) {
+
+    }
+  };
+
+  private View.OnClickListener buttonSampleGWatchListener = new View.OnClickListener() {
+    @Override public void onClick(View v) {
+
+    }
+  };
+
+  private View.OnClickListener buttonSampleStrictModeListener = new View.OnClickListener() {
+    @Override public void onClick(View v) {
+
+    }
+  };
+
+  private View.OnClickListener buttonSampleGandalfStatsListener = new View.OnClickListener() {
+    @Override public void onClick(View v) {
+
+    }
+  };
+}
